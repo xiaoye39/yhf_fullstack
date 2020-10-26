12da6852d8acb26f18f4bb0bcefd52165c6b1592
xiaoye39 <2425635371@qq.com>
2020-10-26 20:53:27 +0800

    微信小程序

--- getUserInfo/user.js
+++
@@ -1,1 +0,0 @@
-var getUserTemplate = function (user) { return "<a class=\"weui-cell  weui-cell_access\" href=\"javascript:\">\n<div class=\"weui-cell__bd\">\n    <p>" + user.name + "</p>\n</div>\n<div class=\"weui-cell__ft\">" + user.company + "</div>\n</a>"; };
