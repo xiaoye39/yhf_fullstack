4d00aa7dc49ec11b88098edec501c4eb90ea5c8d
xiaoye39 <2425635371@qq.com>
2020-10-28 17:11:05 +0800

    小程序

--- wxApp/douban-movie/pages/home/home.js
+++ wxApp/douban-movie/pages/home/home.js
@@ -12,9 +12,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCity(() => {
+    })
   },
-
+  getCity(succeed) {
+    // 先拿到城市名称
+    // 先拿到当前所在地的经纬度
+    wx.getLocation({
+      success: (res) => {
+        // console.log(res);
+        // 将经纬度转换成具体的城市
+        wx.request({
+          url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+          data: {
+            ak: 'Si5H6d9O9Uy7T73vOdvL5Gi9VQP46zF3',
+            output: 'json',
+            coordtype: 'wgs84',
+            location: `${res.latitude}, ${res.longitude}`
+          },
+          success: (res) => {
+            console.log(res);
+            // 拿豆瓣的api地址做接口请求
+            // 将获取到的城市名传给豆瓣api
+            // 拿到当前城市热映电影数据
+          }
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
