670c2a8b1e94008f4061b44fdb0402eb54716dd9
xiaoye39 <2425635371@qq.com>
2020-10-29 15:01:30 +0800

    小程序开发

--- wxApp/douban-movie/pages/home/home.js
+++ wxApp/douban-movie/pages/home/home.js
@@ -5,16 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    allMovies: [
+      {
+        title: "院线热映",
+        url: "/v2/movie/in_theaters",
+        movies: []
+      },
+      {
+        title: "新片榜",
+        url: "/v2/movie/new_movies",
+        movies: []
+      },
+      {
+        title: "口碑榜",
+        url: "/v2/movie/weekly",
+        movies: []
+      },
+      {
+        title: "北美票房榜",
+        url: "/v2/movie/us_box",
+        movies: []
+      },
+      {
+        title: "Top250",
+        url: "/v2/movie/top250",
+        movies: []
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCity(() => {
+    this.getCity((city) => {
+      this.loadData(0, { city: city, apikey: '0df993c66c0c636e29ecbb5344252a4a'})
     })
   },
+
+  loadData(idx, params) {
+    let obj = this.data.allMovies[idx]
+    let url = wx.db.url(obj.url)
+    wx.request({
+      url: url,
+      data: params,
+      header: {}
+    })
+  },
+  
   getCity(succeed) {
     // 先拿到城市名称
     // 先拿到当前所在地的经纬度
@@ -35,6 +73,12 @@ Page({
             // 拿豆瓣的api地址做接口请求
             // 将获取到的城市名传给豆瓣api
             // 拿到当前城市热映电影数据
+            let city = res.data.result.addressComponent.city
+            succeed && succeed(city)
+            succeed(city)
+          },
+          fail: (err) => {
+            console.log(err);
           }
         })
       }
