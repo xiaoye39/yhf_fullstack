3c64a223b7626e30cde493fbbe0746bb8fc2613c
xiaoye39 <2425635371@qq.com>
2020-10-29 14:40:24 +0800

    first commit

--- datastruct/stack/Example.ts
+++ datastruct/stack/Example.ts
@@ -1,4 +1,5 @@
 import Stack from './lib/Stack'; // es6 模块化引入
+import ObjStack from './lib/StackObj';
 const stack = new Stack();
 stack.push("第一条数据");
 stack.push("第二条数据");
@@ -10,23 +11,45 @@ console.log(stack.isEmpty());
 console.log(stack.toString());
 console.log(stack.clear());
 
-const decimalToBinary = function(decNumber:number) {
+const decimalToBinary = function (decNumber: number) {
     let stack = new Stack(); // 入栈每个位
     let number = decNumber;
-    let rem; // 余数
+    let rem;//余数
     let binaryString = "";
     while (number > 0) {
         rem = Math.floor(number % 2);
         stack.push(rem)
-        number = Math.floor(number / 2);
+        number = Math.floor(number / 2)
     }
-    while(!stack.isEmpty()) {
+    while (!stack.isEmpty()) {
         binaryString += stack.pop().toString();
     }
     return binaryString
 }
-const testNumber = 999989993232287;
-console.time("数据栈");
-console.log(decimalToBinary(testNumber));
-console.timeEnd("数据栈");
 
+const decimalToBinaryObjStack = function (decNumber) {
+    const stack = new ObjStack();
+    let number = decNumber;
+    let rem;
+    let binaryString = "";
+    while (number > 0) {
+        rem = Math.floor(number % 2);
+        stack.push(rem)
+        number = Math.floor(number / 2)
+    }
+    while (!stack.isEmpty()) {
+        binaryString += stack.pop().toString();
+    }
+    return binaryString
+
+}
+const testNumber = 999989993232287;
+// const testNumber = 4;
+console.time("数组栈");
+console.log(decimalToBinary(testNumber))
+console.timeEnd("数组栈")
+
+console.time("对象栈");
+const testObjNumber = 999989993232287;
+console.log(decimalToBinaryObjStack(testObjNumber));
+console.timeEnd("对象栈");
