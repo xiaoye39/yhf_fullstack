7c53e058f37a1326dbe634827916b57105caa30e
xiaoye39 <2425635371@qq.com>
2020-10-30 17:07:00 +0800

    web案例

---
+++ web/社区服务/gUntreated.php
@@ -0,0 +1,109 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+ 
+    <style>
+       body{
+            margin: 0;
+    padding: 0;
+  font-size: 15px;
+font-family: verdana;
+background: #FFFFFF;
+}
+
+table tbody tr{
+    margin:0;
+height: 50px;
+
+}
+
+td.t1{
+    width: 70px;
+}
+td.t2{
+    width: 150px;
+}
+td.t3{
+    width: 200px;
+}
+td.t4{
+    width:100px;
+}
+
+
+</style>
+</head>
+<body>
+
+
+<div class="ques">
+
+<table>
+<thead>
+<tr>
+<td class="t1">问题编号</td>
+<td class="t2">投诉时间</td>
+<td class="t3">内容</td>
+<td class="t4"></td>
+</tr>
+</thead>
+<!-- <form name="myfor" action="gSub.php" method="post"> -->
+<?php
+    header("content-type:text/html;charset=utf-8");
+$fromurl="http://localhost/"; //跳转往这个地址。
+if( $_SERVER['HTTP_REFERER'] == "" )
+ {
+ header("Location:".$fromurl); exit;
+ }
+
+
+session_start();
+require('pdo.php');
+$pdo->exec("set names utf8");
+$_SESSION['url1']="gUntreated.php";
+$sql = "SELECT No,complainTime,content,result FROM complaincenter  WHERE station='未处理'";
+
+$rs=$pdo->query($sql);
+
+$list=$rs->fetchall(pdo::FETCH_ASSOC);
+?>
+
+
+
+
+
+<tbody>
+<?php
+foreach($list as $key=>$value)
+{?>
+
+<tr>
+<td class="t1"><a href="gSub01.php?id=<?=$value['No']?>"><?=$value['No']?></a></td>
+<td class="t2"><?=$value['complainTime']?></td>
+<td class="t3"><a href="gSub01.php?id=<?=$value['No']?>"><?=$value['content']?></a></td>
+<td class="t4"><a href="gSub.php?id=<?=$value['No']?>">回复</a></td>
+</td>
+
+</tr>
+
+
+<?php
+}
+$_SESSION['url3']="gUntreated.php";
+?>
+
+</tbody>
+
+
+
+
+</table>
+
+
+<!-- 
+</form> -->
+</div>
+</body>
+</html>
---
+++ web/社区服务/view.php
@@ -0,0 +1,101 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>详细信息查看</title>
+<style>
+body{ font-family: '微软雅黑','文泉驿正黑','黑体';
+    font-size: 20px;
+    background: #ffffff;
+}
+a{
+    text-decoration: none;
+}
+
+</style>
+</head>
+<body>
+
+
+<?php
+    header("content-type:text/html;charset=utf-8");
+$fromurl="http://localhost/"; //跳转往这个地址。
+if( $_SERVER['HTTP_REFERER'] == "" )
+ {
+ header("Location:".$fromurl); exit;
+ }
+
+     if (!isset($_SESSION)) {
+    session_start();
+}
+   $No=$_GET['id']; 
+   
+
+ /*  $url_page=$_SERVER["REQUEST_URI"];*/
+$url1=$_SESSION['url1'];
+require('pdo.php');
+$pdo->exec("set names utf8");
+
+$sql = "SELECT * FROM complaincenter WHERE No=$No ";
+$rs=$pdo->query($sql);
+/*var_dump($rs);*/
+
+$list=$rs->fetchall(pdo::FETCH_ASSOC);
+/*var_dump($list);
+exit();*/
+?>
+
+<table>
+<tbody>
+<?php
+foreach($list as $key=>$value)
+{?>
+<tr>
+<td >投诉编号:</td>
+<td name="No"><?=$value['No']?> </td>
+</tr>
+<tr>
+
+
+<td>投诉人:</td>
+<td  name="userName" ><?=$value['userName']?> </td>
+</tr>
+<tr>
+    <td>投诉时间:</td>
+   <td name="complainTime"><?=$value['complainTime']?></td>
+</tr>
+
+<tr>
+    <td>投诉内容：</td>
+    <td name="content"><?=$value['content']?></td>
+</tr>
+<tr>
+    <td>状态：</td>
+    <td name="station" ><?=$value['station']?></td>
+</tr>
+
+
+  <tr>
+<td>回复人:</td>
+<td  name="offer"> <?=$value['offer']?></td>
+</tr>
+<tr>
+
+<td>内容回复:</td>
+<td  name="result"> <?=$value['result']?></td>
+</tr>
+<tr>
+    <td>回复时间:</td>
+    <td name="handleTime" readonly= "true "><?=$value['handleTime']?></td>
+</tr>
+<?php
+}
+?>
+</tbody>
+</table>
+<!-- echo "<script>alert('退出成功!');location.href='".$_SERVER["HTTP_REFERER"]."';</script>"; -->
+
+<a href=<?php echo $url1; ?> >返回</a>
+
+</body>
+</html>
