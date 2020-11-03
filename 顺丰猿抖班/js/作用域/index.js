8f49adc36480218fe6e35454f9c8144badfa2a37
xiaoye39 <2425635371@qq.com>
2020-11-03 23:31:09 +0800

    作用域

---
+++ 顺丰猿抖班/js/作用域/index.js
@@ -0,0 +1,18 @@
+// function doSomething(a) {
+
+//     function doSomethingElse(c) {
+//         return c - 1
+//     }
+
+//     b = a + doSomethingElse(a * 2)
+//     var b
+
+//     console.log(b * 3);
+// }
+
+// doSomething(2)
+
+
+var a = 2
+var a = 3
+console.log(a);
