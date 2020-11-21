0afd0f013a7f87648ffa59a7e2ecf6263ee4790f
xiaoye39 <2425635371@qq.com>
2020-11-21 14:05:18 +0800

    web案例-社区服务管理系统

---
+++ web/社区服务/continue.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+<style>
+</style>
+</head>
+<body>
+<table>
+<tbody>
+<td>投诉人：</td>
+<td name="userName"></td>
+</tr>
+<tr>
+    <td>投诉时间:</td>
+   <td name="complainTime"></td>
+</tr>
+
+<tr>
+<td>联系地址：</td>
+<td name="addr"></td>
+</tr>
+
+
+<tr>
+    <td>联系电话：</td>
+    <td name="tel"></td>
+</tr>
+
+<tr>
+    <td>投诉内容：</td>
+    <td><textarea readonly="true" name="content"></textarea></td>
+</tr>
+
+
+<tr>
+<td>物业回复:</td>
+<td><textarea name="result"></textarea></td>
+</tr>
+
+<tr>
+<td>回复人:</td>
+<td><input type="text" name="offer"/></td>
+</tr>
+
+<tr>
+<td>回复时间:</td>
+<td><input type="text" name="handleTime" /></td>
+</tr>
+
+</tbody>
+</table>
+</body>
+</html>
---
+++ web/社区服务/logout.php
@@ -0,0 +1,22 @@
+﻿<?php
+    header('Content-type:text/html;charset=utf-8');
+$fromurl="http://localhost/"; //跳转往这个地址。
+if( $_SERVER['HTTP_REFERER'] == "" )
+ {
+ header("Location:".$fromurl); exit;
+ }
+
+     if (!isset($_SESSION)) {
+    session_start();
+}
+$url2=$_SESSION['url2'];
+echo $url2;
+    if(isset($_SESSION['offer'])){
+            session_unset();//free all session variable
+            session_destroy();//销毁一个会话中的全部数据
+            setcookie(session_name(),'',time()-3600);//销毁与客户端的卡号
+            header('location:zhu.html');
+        }else{
+            header("location:$url2");
+        }
+?>
