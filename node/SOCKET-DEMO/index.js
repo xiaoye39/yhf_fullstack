95c7898227890acd56b8c47560f2ddf31e660368
xiaoye39 <2425635371@qq.com>
2020-11-23 16:06:07 +0800

    node-socket

---
+++ node/SOCKET-DEMO/index.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const fs = require('fs')
+const { info } = require('console')
+
+// 搭建一个web服务
+var server = http.createServer(function(req, res) {
+    if (res.url != '/favicon.ico') {
+        res.writeHead(200, {"Content-type": "text/html"})
+        const myreadstream = fs.createReadStream(__dirname + '/views/http_demo.html', 'utf-8')
+        myreadstream.pipe(res)
+    }
+})
+
+var io = require('socket.io')(server)
+
+io.on('connection', function(socket) {  // 声明一个io的使用
+    console.info('一个socket连接成功了');
+    socket.on('link_to_server', (msg) => {
+        console.info(`我收到一个问题 ${msg}`);
+        // 响应客户端
+        socket.emit('link_to_client', 'of course')
+    })
+}) 
+
+server.listen(8888, () => {
+    console.log('server is running...');
+})
