60319f9707b0a46eea358b817f05e95cda686bda
xiaoye39 <2425635371@qq.com>
2020-11-25 22:37:18 +0800

    初识react-脚手架

---
+++ react/comment-app/src/CommentApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
+class CommentApp extends Component {
+    render() {
+        return (
+            <div>
+                {/*评论App*/}
+                <CommentInput/>
+                <CommentList/>
+            </div>
+        )
+    }
+}
+
+export default CommentApp;
---
+++ react/comment-app/src/CommentList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Comment from './Comment';
+class CommentList extends Component {
+    render() {
+        return (
+            <div>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+        )
+    }
+}
+
+export default CommentList;
