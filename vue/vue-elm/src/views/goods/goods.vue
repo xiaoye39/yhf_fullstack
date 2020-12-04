2a3d3fa37b127b63d39cfba3d70fa45653ea87c0
xiaoye39 <2425635371@qq.com>
2020-12-04 18:35:32 +0800

    vue案例-饿了么

--- vue/vue-elm/src/views/goods/goods.vue
+++ vue/vue-elm/src/views/goods/goods.vue
@@ -68,10 +68,9 @@ export default {
       for (let i = 0; i < this.listHeight.length; i++) {
         let height1 = this.listHeight[i]
         let height2 = this.listHeight[i + 1]
-          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
-            return i
-          }
-
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i
+        }
       }
       return 0
     }
