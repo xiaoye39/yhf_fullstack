a8b300e56849a23108ef0ad7d32a596c4b2374aa
xiaoye39 <2425635371@qq.com>
2020-12-12 19:09:51 +0800

    vue状态管理器-vuex

--- vue/vuex/src/components/count.vue
+++ vue/vuex/src/components/count.vue
@@ -2,7 +2,7 @@
   <div class="count">
     <h3>{{$store.state.count}}</h3>
     <p>
-      <button @click="">add</button>
+      <button @click="$store.mutation">add</button>
       <button>reduce</button>
     </p>
   </div>
