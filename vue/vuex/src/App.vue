c6526df6cb134954d5b8a5ff3a54dc11628c9af1
xiaoye39 <2425635371@qq.com>
2020-12-12 19:06:55 +0800

    vue状态管理器-vuex

--- vue/vuex/src/App.vue
+++ vue/vuex/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Count></Count>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Count from '@/components/count.vue' 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Count
   }
 }
 </script>
---
+++ vue/vuex/src/components/count.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="count">
+    <h3>{{$store.state.count}}</h3>
+    <p>
+      <button @click="">add</button>
+      <button>reduce</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
