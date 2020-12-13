05475bd56fce90566f16ecad97ae4a5a5c54d12d
xiaoye39 <2425635371@qq.com>
2020-12-13 11:52:14 +0800

    vuex实战

--- vue/happy-fri/src/components/itemContainer.vue
+++ vue/happy-fri/src/components/itemContainer.vue
@@ -13,8 +13,12 @@
 
 <script>
 export default {
-  props: ['fatherComponent']
-  
+  props: ['fatherComponent'],
+  created() {
+    if (this.fatherComponent === 'home') {
+      document.body.style.backgroundImage = 'url(./)'
+    }
+  }
 }
 </script>
 
