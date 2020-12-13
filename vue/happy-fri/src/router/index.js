bdcc9ec6e5a4b158bd87733a16dd344827cabfa4
xiaoye39 <2425635371@qq.com>
2020-12-13 11:32:34 +0800

    vuex实战

--- vue/happy-fri/src/router/index.js
+++ vue/happy-fri/src/router/index.js
@@ -4,6 +4,11 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home')
+  }
 ]
 
 const router = new VueRouter({
---
+++ vue/happy-fri/src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home_container">
+    <item-container father-component="home"></item-container>
+  </div>
+</template>
+
+<script>
+import itemContainer from '@/components/itemContainer'
+export default {
+  components: {
+    itemContainer
+  }
+}
+</script>
+
+<style>
+
+</style>
