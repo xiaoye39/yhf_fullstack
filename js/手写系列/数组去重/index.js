519ec34e4b5370b2f5326054c803673a8a26a8f4
xiaoye39 <2425635371@qq.com>
2021-01-08 12:17:33 +0800

    js-数组去重

--- js/手写系列/数组去重/index.js
+++ js/手写系列/数组去重/index.js
@@ -1,1 +1,10 @@
  let array = [1, 2, 3, 1, '1', '1']
+
+ function unique(arr) {
+   let res = []
+   for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < res.length; j++) {
+      
+    }
+   }
+ }
