2a03ff7b54f085c0fe61d376d17bd3fc13e08a83
xiaoye39 <2425635371@qq.com>
2021-01-08 15:58:44 +0800

    js-数组去重

--- js/手写系列/数组去重/indexOf.js
+++ js/手写系列/数组去重/indexOf.js
@@ -1,14 +1,30 @@
 let array = [1, 2, 3, 1, '1', '1']
 
+// function unique(arr) {
+//   let res = []
+//   for (let i = 0; i < arr.length; i++) {
+//     let current = arr[i]
+//     if (res.indexOf(current) === -1) {
+//       res.push(current)
+//     }
+//   }
+//   return res
+// }
+
+// console.log(unique(array));
+
+
+
+//排序后去重
 function unique(arr) {
   let res = []
-  for (let i = 0; i < arr.length; i++) {
-    let current = arr[i]
-    if (res.indexOf(current) === -1) {
-      res.push(current)
+  let sortedArr = arr.concat().sort()
+  let seen
+  for (let i = 0; i < sortedArr.length; i++) {
+    // 如果是第一个元素或者相邻的元素不相同
+    if (!i || seen !== sortedArr[i]) {
+      res.push(sortedArr[i])
     }
+    seen = sortedArr[i]
   }
-  return res
 }
-
-console.log(unique(array));
