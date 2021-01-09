9f02bda2ddfc6b3a29efe59640d3b4eac5cc95f3
xiaoye39 <2425635371@qq.com>
2021-01-09 14:56:03 +0800

    js-深浅拷贝

--- js/手写系列/深浅拷贝/index.js
+++ js/手写系列/深浅拷贝/index.js
@@ -1,3 +1,17 @@
-let arr = ['old', 1, true, null, undefined]
-let new_arr = arr.concat()
+// let arr = [{old: 'old'}, 1, true, null, undefined]
+// let new_arr = arr.concat()
+// arr[0].old = 'new'
+// console.log(arr);
+// console.log(new_arr);
+
+
+// let arr = ['old', 1, true, null, undefined]
+// let new_arr = arr.slice()
+// arr.[0].old = 'new'
+
+// console.log(new_arr); 
+
+let arr = [{ old: 'old' }, 1, true, null, undefined]
+let new_arr = JSON.parse(JSON.stringify(arr))
+arr[0].old = 'new'
 console.log(new_arr);
