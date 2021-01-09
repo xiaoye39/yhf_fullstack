96738b481613775c93b56c9ba013b2411f728f8d
xiaoye39 <2425635371@qq.com>
2021-01-09 18:55:18 +0800

    js-深浅拷贝

--- js/手写系列/深浅拷贝/deep.js
+++ js/手写系列/深浅拷贝/deep.js
@@ -2,5 +2,10 @@ let deepCopy = function (obj) {
   if (typeof obj !== 'object') return;
   let newObj = obj instanceof Array ? [] : {}
 
-  
+  for (let key in obj) {
+    if (obj.hasOwnproperty(key)) {
+      newObj[key] = typeof obj[key] === 'object' ? deepCopy(obj[key]) : obj[key]
+    }
+  }
+  return newObj
 }
