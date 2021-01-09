4924e3e4ae6ffdf7540ace166edfc8ccc383829a
xiaoye39 <2425635371@qq.com>
2021-01-09 21:09:19 +0800

    js-数组最大/小值

--- js/手写系列/数组最大-小值/index.js
+++ js/手写系列/数组最大-小值/index.js
@@ -1,3 +1,40 @@
-let array = [1, 2, 3, 2, 0, 5, '6']
-console.log(Math.max(...array));
-console.log(Math.min(...array));
+// let array = [1, 2, 3, 2, 0, 5, '6', undefined]
+// console.log(Math.max(...array));
+// console.log(Math.min(...array));
+
+// console.log(Math.max(true, '2', null));  //2
+// console.log(Math.min(1, '0', {}));  //NaN
+
+// console.log(Math.max() > Math.min());
+// console.log();
+
+
+// 原始方法
+let array = [1, 2, 3, 2, 0, 5, 8]
+// function max (arr) {
+//   let max = arr[0]
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i]
+//     }
+//   }
+//   return max
+// }
+
+
+// reduce
+function max (prev, next) {
+  return Math.max(prev, next)
+}
+
+console.log(array.reduce(max));
+
+
+// 排序
+array.sort(function (a, b) {
+  return b - a
+})
+console.log(array[0]);
+
+
+// eval
