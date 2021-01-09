b7230cd09ddd3e63a7771558d954493ffdd63bca
xiaoye39 <2425635371@qq.com>
2021-01-09 16:20:05 +0800

    js-深浅拷贝

--- js/手写系列/深浅拷贝/shallow.js
+++ js/手写系列/深浅拷贝/shallow.js
@@ -3,5 +3,7 @@ let shallowCopy = function (obj) {
   if (typeof obj !== 'object') return;
   // 根据obj的类型判断是新建一个数组还是对象
   let newObj = obj instanceof Array ? [] : {}
-  
+  for (let key in obj) {
+    newObj[key] = obj[key]
+  }
 }
