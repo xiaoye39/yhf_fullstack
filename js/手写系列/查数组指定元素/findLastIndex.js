6f939ed8f3be9581daa3bebf27a74b59582a9a93
xiaoye39 <2425635371@qq.com>
2021-01-10 13:54:06 +0800

    js-查数组指定元素

--- js/手写系列/查数组指定元素/findLastIndex.js
+++ js/手写系列/查数组指定元素/findLastIndex.js
@@ -4,7 +4,11 @@
 
 
 function findLastIndex(array, predicate, context) {
-
+  let length = array.length;
+  for (let i = length - 1; i >= 0; i--) {
+    if (predicate.call(context, array[i], i, array)) return i
+  }
+  return -1;
 }
 
 console.log(findLastIndex([1, 2, 3, 4], function(item, index, array) {
