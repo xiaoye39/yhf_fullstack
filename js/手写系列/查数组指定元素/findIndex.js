77fcc588f332fd4656d0f21f7294dc1f8757cfaa
xiaoye39 <2425635371@qq.com>
2021-01-10 11:58:35 +0800

    js-查数组指定元素

--- js/手写系列/查数组指定元素/findIndex.js
+++ js/手写系列/查数组指定元素/findIndex.js
@@ -1,3 +1,14 @@
 function isBigEnough(element) {
   return element >= 15
 }
+console.log([12, 1, 5, 8, 123, 44].findIndex(isBigEnough));
+
+// 实现
+function findIndex(array, predicate, context) {
+  for (let i = 0; i < array.length; i++) {
+    predicate(array[i], i, array)
+  }
+}
+console.log(findIndex([1, 2, 3, 4], function(item, i, array) {
+  if (item == 3) return true
+}));  // 2
