b2d2ab8bf6c6252b4e4f1d7a5f34c6391a0d2dff
xiaoye39 <2425635371@qq.com>
2021-01-10 17:54:05 +0800

    js-柯里化

--- js/柯里化/index.js
+++ js/柯里化/index.js
@@ -10,6 +10,42 @@ addCurry(1)(2)  // 3
 
 
 
-function ajax() {
-  
+function ajax(type, url, data) {
+  let xhr = new XMLHttpRequest()
+  xhr.open(type, url, true)
+  xhr.send(data)
 }
+
+ajax('POST', 'www.test.com', 'name="wn"')
+ajax('POST', 'www.test2.com', 'name="wn"')
+ajax('POST', 'www.test3.com', 'name="wn"')
+
+// 利用curry
+let ajaxCurry = curry(ajax)
+
+// 以post类型请求
+let post = ajaxCurry('POST')
+// post('www.test.com', 'name="wn"')
+
+let postFormTest = post('www.test.com')
+postFormTest('name="wn"')
+
+
+
+
+let person = [{name: 'jiushen'}, {name: 'taotao'}]
+let name = person.map(function(item) {
+  return item.name
+})
+
+let prop = curry(function(key, obj) {
+  return obj[key]
+})
+let name2 = person.map(prop('name'))
+
+
+
+
+
+
+ 
