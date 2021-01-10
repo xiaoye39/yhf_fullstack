26096cbd84a3d278005a0ed789bd2e1db36cb319
xiaoye39 <2425635371@qq.com>
2021-01-10 15:03:25 +0800

    js-查数组指定元素

--- js/手写系列/查数组指定元素/sortedIndex.js
+++ js/手写系列/查数组指定元素/sortedIndex.js
@@ -1,1 +1,15 @@
-sortedIndex([10, 20, 30], 25) // 2
+function sortedIndex(array, obj) {
+  let low = 0, high = array.length;
+
+  while (low < high) {
+    let mid = Math.floor((low + high) / 2)
+    if (array[mid] < obj) low = mid + 1
+    else high = mid
+  }
+
+  return high
+}
+
+
+console.log(sortedIndex([10, 20, 30], 25));
+// sortedIndex([10, 20, 30], 25) // 2
