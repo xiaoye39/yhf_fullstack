ecf181603f760cc7a13daac082382f1b691c3961
xiaoye39 <2425635371@qq.com>
2021-01-11 13:13:27 +0800

    手写vue组件发布到npm库

---
+++ vue/verify-code/src/main.js
@@ -0,0 +1,3 @@
+import VerifyCode from './verify-code.vue'
+VerifyCode.install = Vue => Vue.component(VerifyCode.name, VerifyCode)
+export default VerifyCode
---
+++ vue/verify-code/src/verify-code.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="vcWrap">
+    <input v-for="n in len" :key="'codeInput' + n">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['length'],
+  data () {
+    return {
+      len: this.length || 4, // 验证码长度
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
