1766e18126fd77a9798f39314a4a5e54fcff9aee
xiaoye39 <2425635371@qq.com>
2021-01-12 10:35:33 +0800

    vue3+vant电商实战

---
+++ vue/new-bee/src/views/ProductDetail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="product-detail">
+    <s-header :name="商品详情"></s-header>
+    <div class="detail-content">
+      <div class="detail-swipe-wrap">
+        <van-swipe class="my-swipe" indicator-color="#1baeae">
+          <van-swipe-item v-for="(item, index) in detail.goodsCarouselList" :key="index">
+            <img :src="item" alt="">
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+      <div class="product-info">
+        <div class="product-title">
+          {{ detail.goodsName || '' }}
+        </div>
+        <div class="product-desc">免邮费 顺丰快递</div>
+        <div class="product-price">
+          <span>¥{{ detail.sellingPrice || '' }}</span>
+          <!-- <span>库存203</span> -->
+        </div>
+      </div>
+      <div class="product-intro">
+        <ul>
+          <li>概述</li>
+          <li>参数</li>
+          <li>安装服务</li>
+          <li>常见问题</li>
+        </ul>
+        <div class="product-content" v-html="detail.goodsDetailContent || ''"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sHeader from '@/components/SimpleHeader'
+export default {
+  components: {
+    sHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
