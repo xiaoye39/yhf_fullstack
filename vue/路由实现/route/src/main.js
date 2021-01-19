823b1b3549a93ae134c30bff4205fcfdc240356b
xiaoye39 <2425635371@qq.com>
2021-01-19 14:17:33 +0800

    vue-路由实现原理

--- vue/路由实现/route/src/main.js
+++ vue/路由实现/route/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+// Vue.use() 的一个原则是use的对象必须具备install这个方法
 new Vue({
   router,
   render: function (h) { return h(App) }
