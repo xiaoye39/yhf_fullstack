2f542421e15f3e558ebd5fe3d7719812a863fdb6
xiaoye39 <2425635371@qq.com>
2021-01-19 17:06:46 +0800

    vue-路由实现原理

--- vue/路由实现/route/src/router/myVueRouter.js
+++ vue/路由实现/route/src/router/myVueRouter.js
@@ -1,12 +1,41 @@
 let Vue = null
+class HistoryRoute {
+  constructor () {
+    this.current = null
+  }
+}
 class VueRouter {
   constructor (options) {
     this.mode = options.mode || 'hash'
-    this.routes = options.routes
+    this.routes = options.routes || []
+    this.routesMap = this.createMap(this.routes)
+
+    this.history = new HistoryRoute()
+  }
+  createMap(routes) {
+    return routes.reduce((pre, current) => {
+      pre[current.path] = current.component  // { './home': Home }
+      return pre
+    }, {})
   }
 }
 VueRouter.install = function() {
   Vue = v 
+  Vue.mixin({
+    beforeCreate() {
+      if (this.$options && this.$options.router) { // 如果是根组件
+        this._root = this
+        this._router = this.$options.router
+      } else {  // 如果是子组件
+        this._root = this.$parent && this.$parent._root
+      }
+      Object.defineProperty(this, '$router', {
+        get () {
+          return this._root._router
+        }
+      })
+    }
+  })
   Vue.component('router-link', {
     render(h) {
       return h('a', {}, '首页')
