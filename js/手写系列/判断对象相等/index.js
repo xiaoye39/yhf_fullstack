5577351758f8b90adcd0c26fa1bb4f6100aff535
xiaoye39 <2425635371@qq.com>
2021-01-20 18:23:44 +0800

    js-判断对象相等

--- js/手写系列/判断对象相等/index.js
+++ js/手写系列/判断对象相等/index.js
@@ -14,7 +14,7 @@
 // (-0).toString() // '0' 
 
 
-function eq(a, b) {
+function eq(a, b, aStack, bStack) {
   // +0,-0
   if (a === b) return a !== 0 || 1 / a === 1 / b
   // NaN
@@ -28,7 +28,7 @@ function eq(a, b) {
   if (type !== 'function' && type !== 'object' && typeof(b) !== 'object') return false
 
   // 更复杂的对象
-  return deepEq(a, b)
+  return deepEq(a, b, aStack, bStack)
 }
 
 // console.log(NaN, NaN); // true
@@ -61,27 +61,71 @@ function deepEq(a, b) {
     if (typeof a !== 'object' || typeof b !== 'object') return false
 
     let aCtor = a.__proto__.constructor, bCtor = b.__proto__.constructor
-    // aCtor 和 bCtor
+    // aCtor 和 bCtor必须都不是Object构造函数的情况，aCtor 不等于bCtor，那么两个对象一定不相等
     if (aCtor !== bCtor && !(isFunction(aCtor) && aCtor instanceof aCtor && isFunction(bCtor) && bCtor instanceof bCtor) && ('constructor' in a && 'constructor' in b)) {
       return false
     }
   }
+
+  aStack = aStack || []
+  bStack = bStack || []
+  let length = aStack.length
+  // 检查是否有循环引用的部分
+  while (length--) {
+    if (aStack[length] === a) {
+      return bStack[length] === b
+    }
+  }
+  aStack.push(a)
+  bStack.push(b)
+
+  if (areArrays) {
+    length = a.length
+    if (length !== b.length) return false
+    while(length--) {
+      if (!eq(a[length], b[length], aStack)) return false
+    }
+  } else {
+    let keys = Object.keys(a), key
+    length = keys.length
+    if (Object.keys(b).length !== length) return false
+
+    while(length--) {
+      key = keys[length]
+      if (!(b.hasOwnProperty(key) && eq(a[key], b[key], aStack, bStack))) return false
+    }
+  }
+  aStack.pop()
+  bStack.pop()
+  return true
 }
   
 
-function Person(name) {
-  this.name = name
-}
-function Animal(name) {
-  this.name = name
-}
-let person = new Person('Tom')
-let animal = new Animal('Tom')
+// function Person(name) {
+//   this.name = name
+// }
+// function Animal(name) {
+//   this.name = name
+// }
+// let person = new Person('Tom')
+// let animal = new Animal('Tom')
 
-console.log(eq(person, animal));
+// console.log(eq(person, animal));
 
 
-let attrs = Object.create(null)
-attrs.name = 'Jerry'
+// let attrs = Object.create(null)
+// attrs.name = 'Jerry'
 
-console.log(eq(attrs, {name: 'Jerry'}));
+// console.log(eq(attrs, {name: 'Jerry'}));
+
+// a = {abc: null}
+// b = {abc: null}
+// a.abc = a
+// b.abc = b
+
+a = {foo: {b: {foo: {c: {foo: null}}}}}
+b = {foo: {b: {foo: {c: {foo: null}}}}}
+a.foo.b.foo.c.foo = a
+b.foo.b.foo.c.foo = b
+
+console.log(eq(a, b));
