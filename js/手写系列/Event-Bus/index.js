3bca8a98e8ce63c3114da150bfc85477028741cd
xiaoye39 <2425635371@qq.com>
2021-01-22 15:33:04 +0800

    js-发布订阅模式

--- js/手写系列/Event-Bus/index.js
+++ js/手写系列/Event-Bus/index.js
@@ -1,1 +1,33 @@
-class
+class EventEmitter {
+  constructor () {
+    // 存储事件
+    this.events = this.events || new Map()
+    // {
+    //   'ages': function fn() {}
+    // }
+  }
+
+  // 监听事件
+  addListener (type, fn) {
+    if (!this.events.get(type)) {
+      this.events.set(type, fn)
+    }
+  }
+
+  // 触发事件
+  emit (type) {
+    let handle = this.events.get(type)
+    handle.apply(this, [...arguments].slice(1))
+  }
+}
+
+let emitter = new EventEmitter()
+
+emitter.addListener('ages', age => {
+  console.log(age);
+})
+
+emitter.emit('ages', 18)
+
+
+// this.$emit('handle', 123)
