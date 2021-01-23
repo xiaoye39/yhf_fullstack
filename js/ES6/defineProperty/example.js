cebb0abad8b6585f70c0bda8eb1b9cbd93618c4b
xiaoye39 <2425635371@qq.com>
2021-01-23 12:43:10 +0800

    es6新增语法-defineProperty

--- js/ES6/defineProperty/example.js
+++ js/ES6/defineProperty/example.js
@@ -1,14 +1,25 @@
-let obj = {}, value = null;
-Object.defineProperty(obj, 'num', {
-  get: function() {
-    console.log('执行get操作');
-    return value
-  },
-  set: function(newValue) {
-    console.log('执行set操作');
-    value = newValue
-  }
-})
+function Archiver() {
+  let value = null;
+  let archive = [];
 
-obj.num = 2
-console.log(obj.num);
+  Object.defineProperty(obj, 'num', {
+    get: function () {
+      console.log('执行get操作');
+      return value
+    },
+    set: function(newValue) {
+      console.log('执行set操作');
+      archive.push({ val: newValue })
+    }
+  })
+
+  this.getArchiver = function() {
+    return archive
+  }
+}
+
+let arc = new Archiver()
+arc.num  // get
+arc.num = 11 // get
+arc.num = 22 // get
+arc.getArchiver() //[{val: 11}, {val: 22}]
