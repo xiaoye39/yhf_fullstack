4b805ae2d62a491fd7b75b7f89c3a7b4514f6843
xiaoye39 <2425635371@qq.com>
2021-04-11 10:12:15 +0800

    js面试题

---
+++ interview/q5.js
@@ -0,0 +1,23 @@
+var name = 'Word!'
+
+(function() {
+  if (typeof name === 'undefined') {
+    var name = 'Jack'
+    console.log('Goodbye' + name);
+  } else {
+    console.log('Hello' + name);
+  }
+})()
+
+// 变量提升是js将声明移动至作用域的顶部
+
+// var name  = 'Word!'
+// (function() {
+//   var name
+//   if (typeof name === 'undefined') {
+//     name = 'Jack'
+//     console.log('Goodbye' + name);
+//   } else {
+//     console.log('Hello' + name);
+//   }
+// })()
