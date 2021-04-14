cf4924bd7be4aad930f2f3a24f6ce89d02968c18
xiaoye39 <2425635371@qq.com>
2021-04-14 10:41:30 +0800

    vue全家桶+koa实战(日记本)

--- vue/nootbook/nootbook-client/src/router/index.js
+++ vue/nootbook/nootbook-client/src/router/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import StarLogin from '@views/login/StarLogin'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/starLogin',
+    name: 'starLogin',
+    component: StarLogin,
+    meta: {
+      title: '登录'
+    }
+  }
 ]
 
 const router = new VueRouter({
---
+++ vue/nootbook/nootbook-client/src/views/login/StarLogin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="star-login">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
