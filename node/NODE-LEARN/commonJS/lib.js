c0eadc7c90dc818c894046bbc96babe4d88d38aa
xiaoye39 <2425635371@qq.com>
2021-04-26 09:24:22 +0800

    node.js

---
+++ node/NODE-LEARN/commonJS/lib.js
@@ -0,0 +1,14 @@
+// console.log('hello world');
+
+// exports.hello = 'world'
+// exports.add = function minus(a, b) {
+//     return a + b
+// }
+
+setTimeout(() => {
+    console.log(exports);
+}, 1000)
+
+// modules.exports = function minus(a, b) {
+//     return a - b
+// }
